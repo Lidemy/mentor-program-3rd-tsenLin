6750717cb77df9da9b1663103267ab74fb5d2621
tsenLin <sss1042@hotmail.com>
2019-04-27 14:57:12 +0800

    week2

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,17 @@
 function reverse(str) {
-  return str;
+  var reverseStr = '';
+  for(var i=str.length-1; i >= 0; i--) {
+    reverseStr += str[i];
+  }
+  console.log(reverseStr);
+  return reverseStr;
 }
 
-reverse('hello');
+reverse('yoyoyo')
+// 正確輸出：oyoyoy
+
+reverse('1abc2')
+// 正確輸出：2cba1
+
+reverse('1,2,3,2,1')
+// 正確輸出：1,2,3,2,1
