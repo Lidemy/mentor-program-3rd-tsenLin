a81bde6245837ed9dcb38988a608f6f934d780e4
tsenLin <sss1042@hotmail.com>
2019-05-02 21:36:22 +0800

    week3 temp

--- homeworks/week3/hw2.js
+++ homeworks/week3/hw2.js
@@ -1,5 +1,19 @@
 function alphaSwap(str) {
-  console.log(str);
+  let newStr = '';
+
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] >= 'A' && str[i] <= 'Z') {
+      newStr += String.fromCharCode(str.charCodeAt(i) + 32);
+    } else if (str[i] >= 'a' && str[i] <= 'z') {
+      newStr += String.fromCharCode(str.charCodeAt(i) - 32);
+    } else {
+      newStr += str[i];
+    }
+  }
+
+  return newStr;
 }
+
+console.log(alphaSwap('nick'));
 
 module.exports = alphaSwap;
--- homeworks/week3/hw2.test.js
+++ homeworks/week3/hw2.test.js
@@ -4,4 +4,12 @@ describe('hw2', () => {
   it('should return correct answer when str = nick', () => {
     expect(alphaSwap('nick')).toBe('NICK');
   });
+
+  it('should return correct answer when str = nICK', () => {
+    expect(alphaSwap('Nick')).toBe('nICK');
+  });
+
+  it('should return correct answer when str = ,HeLLo123', () => {
+    expect(alphaSwap(',hEllO123')).toBe(',HeLLo123');
+  });
 });
