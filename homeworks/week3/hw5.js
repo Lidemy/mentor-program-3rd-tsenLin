75fcc44718d24dc375d8446159fcbf5fa1195250
tsenLin <sss1042@hotmail.com>
2019-05-04 15:18:23 +0800

    week3 完成

--- homeworks/week3/hw5.js
+++ homeworks/week3/hw5.js
@@ -1,5 +1,29 @@
 function add(a, b) {
-  console.log(a, b);
+  let x;
+  let y;
+  if (a.length > b.length) {
+    x = a;
+    y = b;
+  } else {
+    x = b;
+    y = a;
+  }
+
+  let sum = [0];
+  sum = sum.concat(x.split('').map(n => parseInt(n, 10)));
+
+  for (let i = sum.length - 1, j = y.length - 1; j >= 0; i -= 1, j -= 1) {
+    const temp = parseInt(sum[i], 10) + parseInt(y[j], 10);
+
+    sum[i] = temp % 10;
+    sum[i - 1] = parseInt(sum[i - 1], 10) + parseInt(temp / 10, 10);
+  }
+
+  if (sum[0] === 0) {
+    sum.shift();
+  }
+
+  return sum.join('');
 }
 
 module.exports = add;
--- homeworks/week3/hw5.test.js
+++ homeworks/week3/hw5.test.js
@@ -4,4 +4,16 @@ describe('hw5', () => {
   it('should return correct answer when a=111111111111111111111111111111111111 and b=111111111111111111111111111111111111', () => {
     expect(add('111111111111111111111111111111111111', '111111111111111111111111111111111111')).toBe('222222222222222222222222222222222222');
   });
+
+  it('should return correct answer when a=12312383813881381381 and b=129018313819319831', () => {
+    expect(add('12312383813881381381', '129018313819319831')).toBe('12441402127700701212');
+  });
+
+  it('should return correct answer when a=123 and b=5456', () => {
+    expect(add('123', '5456')).toBe('5579');
+  });
+
+  it('should return correct answer when a=978 and b=154', () => {
+    expect(add('978', '154')).toBe('1132');
+  });
 });
